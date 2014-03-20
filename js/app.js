694c8fb5f292ea5d17e8bb02be433db27dce1f4f
Dario Fuentes <darifuentes@gmail.com>
2014-03-20 16:52:47 -0600

    Segunda Modificación

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var appMarvel = angular.module('appMarvel', []);
 
 appMarvel.controller('MarvelListCtrl', function($scope, $http){
-	$http.get('http://gateway.marvel.com:80/v1/public/characters?nameStartsWith=Spid&limit=10&ts=1&apikey=2cf2798f7928f54962306dbe35e4ce02&hash=1199e3de03ab4729560885059d25a057').success(function(data){
+	$http.get('http://gateway.marvel.com:80/v1/public/comics?format=comic&formatType=comic&dateRange=2011-01-01%2C2013-11-11&ts=1&apikey=2cf2798f7928f54962306dbe35e4ce02&hash=1199e3de03ab4729560885059d25a057').success(function(data){
 		$scope.series = data.data.results;
 		console.log(data.data.results);
 	});
